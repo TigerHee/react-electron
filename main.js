c9da9090e2f7f13bd25c2f013c9229199abeffca
hehu <316031602@qq.com>
2019-12-16 21:28:18 +0800

    electron开发环境配置

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+ *  electron 主进程
+ */
+const { app, BrowserWindow, ipcMain } = require('electron')
+// 判断是否开发环境
+const isDev = require('electron-is-dev')
+
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  const urlLocation = isDev ? 'http://localhost:3000' : ''
+  mainWindow.loadURL(urlLocation)
+})
